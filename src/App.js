3433e0b0ac467e93582a5eb9f569387d7ddb3c65
ZQWhiting <zach.whiting@icloud.com>
2020-10-20 13:17:38 -0600

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// import Opening from './pages/Opening';
 
 function App() {
-  return(
-    <div>
-      Welcome.
-    </div>
-  )
+  return (
+		<Router>
+			<Switch>
+				{/* <Route exact path='/' component={Opening} /> */}
+			</Switch>
+		</Router>
+  );
 }
 
 export default App;
